d4313466487c14f237939e32a7abe39737864c5f
saran106 <saranr0313@gmail.com>
2024-08-12 13:06:21 +0530

    empty file

---
+++ checking.js
@@ -0,0 +1,11 @@
+function reversearr(a){
+    var new_array=new Array;
+    for(var i=a.length-1;i>=0;i--){
+        new_array.push(input[i]);
+    }
+    return new_array;
+
+}
+
+var a = [3,5,7,8]
+var b = reverseArr(a);
